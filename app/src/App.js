7eee1da4c70229592e20fcc1c828cba1e47ec32c
Ramyak Singh <singh351@purdue.edu>
2019-07-09 01:05:51 -0400

    Added Wearable metric calculations

--- app/src/App.js
+++ app/src/App.js
@@ -32,16 +32,125 @@ class App extends Component {
   }
 
     componentDidMount() {
-    Tabletop.init({
-      key: '19SNEbgmJqzFkXajdTnCDN5S6-PHmqFIGoN_MCFeOMcc',
-      callback: googleData => {
-        this.props.updateSearchData(googleData["Sheet1"].elements);
 
-      },
-      simpleSheet: false
-    })
+        Tabletop.init({
+          key: '19SNEbgmJqzFkXajdTnCDN5S6-PHmqFIGoN_MCFeOMcc',
+          callback: googleData => {
 
-  }
+            let temp = googleData["Sheet1"].elements;
+
+            let wearability = this.computeWearability(googleData["Wearability"].elements);
+
+            for(let i = 0; i < temp.length; i++) {
+                  temp[i] = {
+                 ...temp[i],
+                 Wearability: wearability[i]["Wearability"]
+               };
+            }
+            this.props.updateSearchData(temp);
+
+
+          },
+          simpleSheet: false
+        })
+
+    }
+
+    computeWearability = (wearability) => {
+
+      wearability = wearability.map((item) => {
+
+        let count = 0
+        let totalCount = 50
+
+        let attachScore = item["Attachment to the Body"];
+
+        switch (attachScore) {
+
+            case "Threads":
+              attachScore = 5;
+              break;
+            case "Velcro":
+              attachScore = 3;
+              break;
+            case "Adhesives":
+               attachScore = 4;
+              break;
+            case "Embedded":
+              attachScore = 2;
+              break;
+            case "Straps":
+              attachScore = 1;
+              break;
+            case "Clips":
+              attachScore = 4;
+              break;
+            default:
+              attachScore = 0;
+              totalCount = totalCount - 5;
+
+        }
+
+        let mainScore = item["Maintenance required"];
+
+        if(isNaN(mainScore)) {
+          totalCount = totalCount - 9;
+        }
+
+        else {
+
+          count += parseInt(mainScore);
+        }
+
+        let actScore = item["activity obstruction"];
+
+        if(isNaN(actScore)) {
+          totalCount = totalCount - 9;
+        }
+
+        else {
+          count += parseInt(actScore);
+        }
+
+        let intScore = item["Integration (form and size)"];
+
+        if(isNaN(intScore)) {
+          totalCount = totalCount - 9;
+        }
+
+        else {
+          count += parseInt(intScore);
+        }
+
+        let conScore = item["Context"];
+
+        if(isNaN(conScore)) {
+          totalCount = totalCount - 9;
+        }
+
+        else {
+          count += parseInt(conScore);
+        }
+
+        let aesScore = item["Aesthetics"];
+
+        if(isNaN(aesScore)) {
+          totalCount = totalCount - 9;
+        }
+
+        else {
+          count += parseInt(aesScore);
+        }
+
+        //sum can be calculated by simple array iteration
+        //total possible = 50 - SummaxofNAcolumns
+        //percentage = sum / total possible
+
+        return {"Wearability": ((count / totalCount) * 100).toFixed(2)}
+      })
+
+      return wearability;
+    }
 
   displayMatrix = () => {
     this.setState({
@@ -54,14 +163,7 @@ class App extends Component {
 
         /*
 
-
-          1) Get UI up to spec
-
-            a) Fix filter - Idk
-            b) Fix searchBar - Idk
-
-          3) Fix the problem where leftpanemodal should get closed by clicking anywhere on the page, also improve it's UI - Idk
-
+          1) Wearability
 
           5) Fix the contribution display in the filter
           5) Designing contact us and contribute links
